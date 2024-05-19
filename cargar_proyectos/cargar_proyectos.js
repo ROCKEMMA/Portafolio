e7121f2443bb8f39a32502d957b3e24b63341363
rockemma <jossuerm55@gmail.com>
2024-05-19 17:32:01 -0600

    Actualización de proyectos

--- cargar_proyectos/cargar_proyectos.js
+++ cargar_proyectos/cargar_proyectos.js
@@ -4,24 +4,37 @@ function cargar_proyectos(){
 
     let div_proyectos_servision = document.querySelector(".proyectos-y-servision");
 
-    lista_servicios.forEach((servicio)=>{
+    let div = document.createElement("div");
+    div.className = "item-servicio";
 
-        let div = document.createElement("div");
-        div.className = "item-servicio";
-        div.innerHTML = `
-            <di class="div-img">
-                <img src="${servicio.img}" alt="">
-            </di>
-            <img class="ico" src="https://cdn.hugeicons.com/icons/laptop-performance-solid-rounded.svg" src="">
-            <span class="tituloEquipos">Equipos</span>
-            <span class="numeroEquipos">${servicio.numeroEquipos}</span>
-            <h3>${servicio.nombreServicio}</h3>
-            <p>${servicio.descripcion}</p>
-            `;
-        div_proyectos_servision.appendChild(div);
+    lista_servicios.forEach((servicio, indice) => {
+        let item = item_mantenimiento(servicio.nombreServicio, servicio.numeroEquipos);
 
+        if (indice % 2 === 0) {
+            item.classList.add('f1');
+        }else {
+            item.classList.add('f2');
+        }
+
+        div.appendChild(item);
     });
 
+    div_proyectos_servision.appendChild(div);
+
+}
+
+function item_mantenimiento(descr,cantidad){
+    const item = document.createElement('div');
+
+    const txt_descripcion = document.createElement('span');
+    txt_descripcion.textContent = descr;
+    const txt_cantidad = document.createElement('span');
+    txt_cantidad.textContent = cantidad;
+
+    item.appendChild(txt_descripcion);
+    item.appendChild(txt_cantidad);
+
+    return item;
 }
 
 export { cargar_proyectos }
